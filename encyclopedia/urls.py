703224dc9d21147506354692f439ad5a285cfe37
maco-data <mcospino@gmail.com>
2022-02-15 23:20:15 +0100

    Add app_name to urls.py file

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:entry_name>", views.show_entry, name="show_entry"),
