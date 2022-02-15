911bc27fe798543424947bd7d2d6f8f82a2f37e7
maco-data <mcospino@gmail.com>
2022-02-15 22:56:21 +0100

    Fix urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:entry_name>", views.show_entry, name="show_entry"),
+    path("search/", views.search_entry, name="search_entry"),
+    path("create/", views.create_entry, name="create_entry"),
+    path("edit/<str:entry_name>", views.edit_entry, name="edit_entry"),
+    path("random/", views.random_entry, name="random_entry")
 ]
